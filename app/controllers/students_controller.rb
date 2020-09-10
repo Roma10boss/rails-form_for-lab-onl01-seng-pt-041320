de98e7eba4caa55ca1da82a47681abb37e85e567
Romario Gustave <romagustave10@gmail.com>
2020-09-10 04:12:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,37 @@
 class StudentsController < ApplicationController
 
-  def index
-    @students = Student.all
-  end
+    def show
+        @student = Student.find(params[:id])
+    end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+    def new
+        @student = Student.new
+    end
 
-  def new
+    def create
+        @student = Student.new(student_params(:first_name, :last_name))
+        @student.save
 
-  end
+		redirect_to student_path(@student)
+    end
 
-  def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path
-  end
+	def update
+		@student = Student.find(params[:id])
+		@student.update(student_params(:first_name, :last_name))
 
+		redirect_to student_path(@student)
 
+	end
+
+	def edit
+	  @student = Student.find(params[:id])
+
+	end
+
+	private
+
+	def student_params(*args)
+		params.require(:student).permit(*args)
+	end
 
 end
